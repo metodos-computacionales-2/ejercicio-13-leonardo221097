c6a5106227e9636ed6c62776bb8903afb1efc8b3
leonardo221097 <leoalca22@gmail.com>
2019-11-13 17:02:47 +0000

    subo ejercicio que calcula el valor pi

---
+++ ejercicio5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+const int VAR= 4;
+int main(void)
+{
+    std::cout<<sin(VAR)<<std::endl;
+    std::cout<<M_PI<<std::endl;
+    std::cout<<M_PI<<std::endl;
+    return 0;
+}
